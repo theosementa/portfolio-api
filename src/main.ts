5201a16e0c02ec789d32dcb1d44a305837c2c9fe
Theo Sementa <theosementa@gmail.com>
2024-12-23 17:32:43 +0100

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
